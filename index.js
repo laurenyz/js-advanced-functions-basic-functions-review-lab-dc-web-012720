2def757c30ff3754ad82a470ace5dee96396046f
Lauren Yu <laurenkyu@gmail.com>
2020-03-28 16:08:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ function saturdayFun(string = "roller-skate") {
 }
 
 function wrapAdjective(punc = "*") {
-  let innerFunction = function(string = "special") {
+  return function (string = "special") {
     return `You are ${punc}${string}${punc}!`
   }
-  
-  return innerFunction()
 }
