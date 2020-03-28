d910395790f169173b467297431c59b41503d2e5
Lauren Yu <laurenkyu@gmail.com>
2020-03-28 16:14:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function wrapAdjective(punc = "*") {
 const Calculator = {
   add: function(a, b){
     return a+b
-  }
+  },
   subtract: function(a, b){
     return a-b
-  }
+  },
   multiply: function(a, b){
     return a*b
-  }
+  },
   divide: function(a, b){
     return a/b
   }
