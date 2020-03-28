3cc9900eee75ea972a62ce417f898aa150cd3f0d
Lauren Yu <laurenkyu@gmail.com>
2020-03-28 16:13:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,13 @@ const Calculator = {
   add: function(a, b){
     return a+b
   }
+  subtract: function(a, b){
+    return a-b
+  }
+  multiply: function(a, b){
+    return a*b
+  }
+  divide: function(a, b){
+    return a/b
+  }
 }
