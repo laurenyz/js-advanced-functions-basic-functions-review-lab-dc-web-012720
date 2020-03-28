140328b1631a97f531e60b56e5b19f1b79df9d4d
Lauren Yu <laurenkyu@gmail.com>
2020-03-28 15:44:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function mondayWork(string = "go to the office"){
+function mondayWork(string = "go to the office") {
   return `This Monday, I will ${string}.`
+}
+
+function saturdayFun(string = "roller-skate") {
+  return `This Saturday, I want to ${string}!`
 }
